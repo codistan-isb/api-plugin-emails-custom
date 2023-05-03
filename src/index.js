ba3c9a7736a24ae8b3559047da4c5182cb67e2e0
shoaibfarooq3 <shoaib.farooq@codistan.org>
2023-05-03 17:38:01 +0500

    New Updation

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ export default async function register(app) {
     //     updatedAt: { type: Date, default: Date.now },
     //     createdAt: { type: Date, default: Date.now },
     //   },
-
     // },
     graphQL: {
       schemas: [mySchema],
