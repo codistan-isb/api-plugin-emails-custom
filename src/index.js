ab9c3a5500c327b6102f1f4c0926a56dfe230c66
shoaibfarooq3 <shoaib.farooq@codistan.org>
2023-05-04 11:24:58 +0500

    phoneNumber field updated

--- src/index.js
+++ src/index.js
@@ -4,24 +4,18 @@ const require = createRequire(import.meta.url);
 const pkg = require("../package.json");
 import myResolvers from "./resolvers/resolvers.js"
 const mySchema = importAsString("./schema/schema.graphql");
-// import mySchema from "./schema/schema.graphql"
+
 /**
  * @summary Import and call this function to add this plugin to your API.
  * @param {Object} app The ReactionAPI instance
  * @returns {undefined}
  */
+
 export default async function register(app) {
   await app.registerPlugin({
     label: pkg.label,
     name: pkg.name,
     version: pkg.version,
-    // collections: {
-    //   Shops: {
-    //     name: "Shops",
-    //     updatedAt: { type: Date, default: Date.now },
-    //     createdAt: { type: Date, default: Date.now },
-    //   },
-    // },
     graphQL: {
       schemas: [mySchema],
       resolvers: myResolvers,
