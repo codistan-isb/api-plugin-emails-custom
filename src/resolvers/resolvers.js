8b57bd6412a22beb16253c753fe984bfc713f39a
shoaibfarooq3 <shoaib.farooq@codistan.org>
2023-05-08 11:02:06 +0500

    Console Clear

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -7,18 +7,18 @@ export default {
                 collections: { Shops },
             } = context;
             const shop = await Shops.findOne({ shopType: "primary" });
-            console.log(shop)
+            // console.log(shop)
             if (!shop) throw new ReactionError("not-found", "Shop not found");
             const emailBody = `Name: ${name}\nEmail: ${email}\nPhone Number: ${phoneNumber}\nMessage: ${message}`;
-            console.log("emailBody:- ", emailBody)
-            console.log("shop email:- ", shop.emails[0].address)
+            // console.log("emailBody:- ", emailBody)
+            // console.log("shop email:- ", shop.emails[0].address)
             const sendEmailResult = await context.mutations.sendEmail(context, {
                 to: shop.emails[0].address,
                 data: emailBody,
                 subject: "Contact Form Submission",
                 fromShop: shop,
             })
-            console.log("sendEmailResult:- ", sendEmailResult)
+            // console.log("sendEmailResult:- ", sendEmailResult)
             if (sendEmailResult) {
                 return true;
             } else {
